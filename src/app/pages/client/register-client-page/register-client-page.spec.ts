7320b5e5bd033d6150531c599cf711f6e1da2e82
mythos <developerwebmythos@gmail.com>
2025-11-03 03:34:10 -0500

    EF003: Create Register Client component page.

---
+++ src/app/pages/client/register-client-page/register-client-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterClientPage } from './register-client-page';
+
+describe('RegisterClientPage', () => {
+  let component: RegisterClientPage;
+  let fixture: ComponentFixture<RegisterClientPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegisterClientPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterClientPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/client/register-client-page/register-client-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-client-page',
+  imports: [],
+  templateUrl: './register-client-page.html',
+  styleUrl: './register-client-page.css'
+})
+export class RegisterClientPage {
+
+}
